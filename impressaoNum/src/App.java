8e1f3d1f4d1f5dc965f9746f577373ebf983eb78
Mari071 <maarisla0@gmail.com>
2025-04-25 14:45:56 -0300

    ImpressaoNumero

---
+++ impressaoNum/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+       public static void main(String[] args){
+
+        int num = 1;
+
+        while (num <= 50){
+            System.out.println(num);
+            num = num + 1;
+
+        }
+        
+    }             
+        
+}
+

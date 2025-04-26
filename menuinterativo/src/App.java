f73a3db62d8a69e4aa7bb81edd15b4134ef5f03c
Mari071 <maarisla0@gmail.com>
2025-04-26 17:42:39 -0300

    menu

---
+++ menuinterativo/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner (System.in);
+
+        int resUsu = 0;
+
+        do {
+            System.out.println("Escolha uma das opções  (1 - Adicionar | 2 - Remover | 3 - Sair)");
+            resUsu = scanner.nextInt();
+
+            if (resUsu == 1){
+                System.out.println("Adicionado!");
+        
+            } if (resUsu == 2) {
+
+                System.out.println("Removido!");
+            }
+
+        }while(resUsu == 1|| resUsu == 2);
+
+    }
+}

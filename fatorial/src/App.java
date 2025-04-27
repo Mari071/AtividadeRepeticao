e3d6104a6a2a7cbff2a8ec71fb22700a76be8de8
Mari071 <maarisla0@gmail.com>
2025-04-27 15:20:33 -0300

    fatorial

---
+++ fatorial/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner (System.in);
+
+        int numU = 0;
+        int resultado = 1;
+
+        System.out.println("Digite um número (positivo): ");
+        numU = scanner.nextInt();
+
+        if (numU < 0){
+            System.out.println("Um número POSITIVO!");
+        } else {
+            for (int i = 1; i <= numU; i++){
+                resultado *= i;
+            }
+            
+            System.out.println("Fatorial do número digitado = " + resultado);
+        }
+
+        scanner.close();
+
+    }
+}

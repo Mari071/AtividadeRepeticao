ba70dee7498a823c45e814d1051b66eb15d5c661
Mari071 <maarisla0@gmail.com>
2025-04-27 16:46:28 -0300

    impar

---
+++ impar/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numimp = 1;
+
+        for (int i = 1; i <= 50; i++){
+            System.out.println(numimp);
+            numimp = numimp + 2;
+            
+        }
+    }
+}
